082b588c525e2dcc20d1f1144aba69d79702d9a6
Bruno Dyego Silveira da Silva <bruno.ufra@yahoo.com.br>
2023-04-20 23:35:53 -0300

    Create animation left

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,7 +1,14 @@
 const btnRigth = document.querySelector('.button-arrow.-rigth');
+const btnLeft = document.querySelector('.button-arrow.-left');
 const elements = document.querySelector('.card-figurre');
 let counter = 0;
+
 btnRigth.addEventListener('click', function() {
     counter = counter+100;
+    elements.style = `transform: translateX(${counter}px)`;
+})
+
+btnLeft.addEventListener('click', function() {
+    counter = counter-100;
     elements.style = `transform: translateX(${counter}px)`;
 })
