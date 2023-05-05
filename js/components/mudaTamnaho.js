d6681551cd69558f4958cc0730a63fd99f9f3cd5
Bruno Dyego Silveira da Silva <bruno.ufra@yahoo.com.br>
2023-05-04 23:53:56 -0300

    add in component mail-ul flex direction column

---
+++ js/components/mudaTamnaho.js
@@ -0,0 +1,7 @@
+  /*  function mudouTamanho() {
+        if(window.innerWidth >= 768){
+            menu.style.display = 'flex'
+        } else {
+            menu.style.display = "none"
+        }
+    }*/
